e51424c6fea50624fca95f6234abfb7a46d750cd
CodingManoj <mvp2612@hotmail.com>
2023-08-04 21:49:09 -0400

    Git Auto Push

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "test" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL
   load_balancer_type = "application"
-  security_groups    = var.INTERNAL ? aws_security_group.alb_private.*.id : [aws_security_group.alb_public.*.id]
+  security_groups    = var.INTERNAL ? aws_security_group.alb_private.*.id : aws_security_group.alb_public.*.id
   subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
 
 #   enable_deletion_protection = true
