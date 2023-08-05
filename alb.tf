e080019fc103808de07f231ae132fefb2b95ef94
CodingManoj <mvp2612@hotmail.com>
2023-08-04 21:35:12 -0400

    Git Auto Push

---
+++ alb.tf
@@ -0,0 +1,20 @@
+# provisions ALB
+resource "aws_lb" "test" {
+  name               = var.ALB_NAME
+  internal           = var.INTERNAL
+  load_balancer_type = "application"
+  security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb_public.id]
+  subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
+
+#   enable_deletion_protection = true
+
+#   access_logs {
+#     bucket  = aws_s3_bucket.lb_logs.id
+#     prefix  = "test-lb"
+#     enabled = true
+#   }
+
+  tags = {
+    Environment = "production"
+  }
+}
---
+++ sg.tf
@@ -0,0 +1,51 @@
+# Public Load Balancer Security Group
+resource "aws_security_group" "alb_public" {
+  name        = "roboshop-${var.ENV}-public-alb-sg"
+  description = "Allows Traffic From Internet"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "http from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-alb-sg"
+  }
+}
+
+# Private Load Balancer Security Group
+resource "aws_security_group" "alb_private" {
+  name        = "roboshop-${var.ENV}-private-alb-sg"
+  description = "Allows Traffic From Intranet"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-alb-sg"
+  }
+}
