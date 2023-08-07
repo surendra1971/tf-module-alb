5987f3f3c6c117977ce33c0db85054c1f610d729
CodingManoj <mvp2612@hotmail.com>
2023-08-06 21:16:10 -0400

    Git Auto Push

---
+++ listener.tf
@@ -0,0 +1,17 @@
+resource "aws_lb_listener" "private" {
+  count             = var.INTERNAL ? 1 : 0
+
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "ALB_ARN" {
     value   = aws_lb.robot_alb.arn
 }
 
+output "LISTERNER_ARN" {
+    value   = aws_lb_listener.private.*.arn
+}
