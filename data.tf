aeff2e88d97857df6e3431ff4bfdcdc4a5a1e5e2
Your Name <you@example.com>
2023-08-09 01:20:33 +0530

    updating the Auto Push

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "b54-tf-remote-state"
+    bucket = "b54-tf-remote-state-file"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
